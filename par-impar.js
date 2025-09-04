bfcddb045649350ac0ef51b3011fbae27e535f17
carolinamoralese <caromorales1396@gmail.com>
2025-09-03 19:25:19 -0500

    feat:logica para averiguar si numero es par o impar

---
+++ par-impar.js
@@ -0,0 +1,17 @@
+//const number = 10;
+
+const rl = require('readline').createInterface({input:process.stdin, output: process.stdout})
+
+rl.question("escribe un numero", (n) => {
+    const number = Number(n)
+    if (isNaN(number)) {
+  console.log("Entrada invalida, no es un número ");
+  rl.close()
+} else if (number % 2 === 0) {
+  console.log("Par");
+} else {
+  console.log("Impar");
+}
+rl.close()
+})
+
